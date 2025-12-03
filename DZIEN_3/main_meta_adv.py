96c5fa917405561f2fa50c9308f0ea9da6d3854c
albim72 <marcin.albiniak72@gmail.com>
2025-12-03 10:07:33 +0100

    Update main_meta_adv.py

--- DZIEN_3/main_meta_adv.py
+++ DZIEN_3/main_meta_adv.py
@@ -9,3 +9,21 @@ if __name__ == '__main__':
         pass
 
     print(f"Registry zawiera: {registry}")
+
+    print("\n==== 2) RequireMethodMeta - wymuszenie metody run() ====\n")
+
+    class Job(BaseRequire):
+        def run(self):
+            print("Uruchomiony Job")
+
+    job = Job()
+    job.run()
+
+
+    # class JobTest(BaseRequire):
+    #     def runs(self):
+    #         print("Uruchomiony JobTest - nie ma metody run()")
+    # 
+    # 
+    # jobt = JobTest()
+    # jobt.runs()

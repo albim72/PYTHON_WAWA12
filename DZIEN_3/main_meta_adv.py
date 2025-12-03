850c0667206b2cd245b294d05713bf4f3a58117e
albim72 <marcin.albiniak72@gmail.com>
2025-12-03 10:22:14 +0100

    Update main_meta_adv.py

--- DZIEN_3/main_meta_adv.py
+++ DZIEN_3/main_meta_adv.py
@@ -23,7 +23,20 @@ if __name__ == '__main__':
     # class JobTest(BaseRequire):
     #     def runs(self):
     #         print("Uruchomiony JobTest - nie ma metody run()")
-    # 
-    # 
+    #
+    #
     # jobt = JobTest()
     # jobt.runs()
+    print("\n==== 3) Autologowanie wszystkich metod ====\n")
+    class Worker(metaclass=AutoLoggedMeta):
+        def process(self, x):
+            print(f"wynik = {x*3}")
+        def ekstra(self,info):
+            print(f"dodatkowe informacje: {info}")
+        def wybor(self,x):
+            print(f"wybrany element parzysty: {x%2==0}")
+
+    w = Worker()
+    w.process(5)
+    w.ekstra("informacje dodatkowe")
+    w.wybor(10)
